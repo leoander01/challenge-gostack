1768d752fa17880bc459daa2ba37d2e881a061e9
Leoander Neves <leoanderneves8@outlook.com>
2020-11-19 09:33:44 -0300

    Desafio 07 - Fundamentos ReactJS

---
+++ gostack-template-fundamentos-reactjs/src/utils/formatValue.ts
@@ -0,0 +1,8 @@
+// Formatar o tipo da moeda (R$)
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); 
+
+export default formatValue;
